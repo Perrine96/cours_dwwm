1057bcc9d411b50eddce018e06d32dc0c2f58868
Perrine96 <perrinemallard96@gmail.com>
2024-09-04 12:17:42 +0200

    update projet hamburger

--- projet_hamburger/public/main.js
+++ projet_hamburger/public/main.js
@@ -8,7 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
     // On récupère la hauteur: 
     // console.log(nav.offsetHeight);
     // On met un padding avec la hauteur récupérée: 
-    main.style.paddingTop = `${nav.offsetHeight + 4}px`;
+    main.style.paddingTop = `${nav.offsetHeight + 5}px`;
+    ulMobile.style.marginTop = `${nav.offsetHeight}px`;
+    ulMobile.style.minHeight = `${window.innerHeight - nav.offsetHeight}px`;
 });
 
 // document.addEventListener('DOMContentLoaded', () => {
@@ -60,27 +62,43 @@ window.addEventListener("scroll", () => {
         }
     }
 });
+
 // opacité
 // window.addEventListener("scroll", () => {
-//     if (window.scrollY > 10) nav.style.opacity = "0";
+//     if (window.scrollY > 10) main.style.opacity = "0";
 //     else if (window.scrollY === 0) nav.style.opacity = "1";
 //   });
 
 // nav ouverte disparaît à partir de 640px
 window.addEventListener("resize", () => {
-    if (window.innerHeight > 640) {
-        main.style.paddingTop = `${nav.offsetHeight + 4}px`;
-        ulMobile.classList.contains('toggled');
-        ulMobile.animate(
-            [{ transform: "translate(0)" }, { transform: "translate(100%)" }],
-            { fill: "forwards", duration: 0 }
-        );
+    main.style.paddingTop = `${nav.offsetHeight + 5}px`;
+    if (window.innerWidth > 640) {
+        if (ulMobile.classList.contains('toggled')); 
+            ulMobile.classList.remove('toggled');
+        {
+            ulMobile.animate(
+                [{ transform: "translate(0)" }, { transform: "translate(100%)" }],
+                { fill: "forwards", duration: 0 }
+            );
+        }
     }
 });
 
+// recalcul vh
 const appHeight = () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
   window.addEventListener('resize', appHeight)
   appHeight()
+
+
+// calcul barre d'accueil 
+  window.addEventListener('resize', () => {
+      ulMobile.style.marginTop = `${nav.offsetHeight}px`
+    });
+
+// nav s'adapte au resize
+window.addEventListener('resize', () => {
+    ulMobile.style.minHeight = `${window.innerHeight - nav.offsetHeight}px`
+});
