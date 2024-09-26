7b98230fefd26b9d75b8ffe04643e9128293200a
Perrine96 <perrinemallard96@gmail.com>
2024-09-26 16:11:56 +0200

    update cours 26/09/24

--- 10.php/fichiers/index.php
+++ 10.php/fichiers/index.php
@@ -1,13 +1,19 @@
 <?php
 
+$personne = (object)["prenom" => "tata", "nom" => "toto", "age" => 16];
+$personne2 = (object)["prenom" => "tata", "nom" => "titi", "age" => 21];
+$tab = [$personne, $personne2];
+$fileName = __DIR__  . "//" . "sauv.csv"; 
 
-
-
-$fileName = __DIR__  . "//" . "sauv.txt"; 
-echo $fileName; 
-
-$file = fopen($fileName, "w");
-
-fwrite($file, "Bonjour");
-
+$header = ["prenom", "nom", "age"];
+function arrayToCsv($fileName, $donnees, $delimiter, $header)
+{
+$file = fopen($fileName, 'w');
+fputcsv($file, $header, $delimiter);
+foreach($donnees as $row) {
+    $row = (array) $row;
+    fputcsv($file, $row, $delimiter);
+    }
 fclose($file);
+}
+arrayToCsv($fileName, $tab, ",", $header);
